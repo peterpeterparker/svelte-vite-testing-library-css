3a11db7780f9189b1e12aafaa5a2a1bfc13b0bed
David Dal Busco <david.dalbusco@outlook.com>
2024-12-09 17:56:59 +0100

    chore: as in doc

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite'
+
+// Svelte test doc: https://svelte.dev/docs/svelte/testing
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit() as never, svelteTesting() as never],
+
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined,
 
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom'
 	}
 });
